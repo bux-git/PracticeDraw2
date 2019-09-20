1401cd20d7a40f88be70292e4ac66916fdac01d9
刘跃明 <471025316@qq.com>
2019-09-20 17:31:25 +0800

    1..init

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice06LightingColorFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice06LightingColorFilterView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -26,20 +28,25 @@ public class Practice06LightingColorFilterView extends View {
     public Practice06LightingColorFilterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
-
+    ColorFilter redFilter;
+    ColorFilter blueFilter;
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        // 使用 Paint.setColorFilter() 来设置 LightingColorFilter
+         redFilter = new LightingColorFilter(0x00ffff, 0x000000);
+        // 使用 Paint.setColorFilter() 来设置 LightingColorFilter
+         blueFilter = new LightingColorFilter(0xffffff, 0x0088800);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // 使用 Paint.setColorFilter() 来设置 LightingColorFilter
 
+        paint.setColorFilter(redFilter);
         // 第一个 LightingColorFilter：去掉红色部分
         canvas.drawBitmap(bitmap, 0, 0, paint);
-
+        paint.setColorFilter(blueFilter);
         // 第二个 LightingColorFilter：增强绿色部分
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 100, 0, paint);
     }
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -2,8 +2,15 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -33,8 +40,22 @@ public class Practice12PathEffectView extends View {
         path.rLineTo(100, 100);
         path.rLineTo(70, -120);
         path.rLineTo(150, 80);
+
+
+        Path path1 = new Path();
+        path1.lineTo(10,10);
+        path1.lineTo(20,0);
+        pathDashEffect = new PathDashPathEffect(path1,30,0, PathDashPathEffect.Style.MORPH);
     }
 
+
+    PathEffect cornerPathEffect = new CornerPathEffect(15);
+    PathEffect discrete = new DiscretePathEffect(20,5);
+    PathEffect dashPath = new DashPathEffect(new float[]{10,3,15,8},50);
+    PathEffect pathDashEffect;
+
+    PathEffect sumPath = new SumPathEffect(cornerPathEffect,dashPath);
+    PathEffect composePath = new ComposePathEffect(dashPath,cornerPathEffect);
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -42,36 +63,44 @@ public class Practice12PathEffectView extends View {
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
         // 第一处：CornerPathEffect
+        paint.setPathEffect(cornerPathEffect);
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
         // 第二处：DiscretePathEffect
+        paint.setPathEffect(discrete);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
         // 第三处：DashPathEffect
+        paint.setPathEffect(dashPath);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 200);
         // 第四处：PathDashPathEffect
+        paint.setPathEffect(pathDashEffect);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
         // 第五处：SumPathEffect
+        paint.setPathEffect(sumPath);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
         // 第六处：ComposePathEffect
+        paint.setPathEffect(composePath);
         canvas.drawPath(path, paint);
         canvas.restore();
+
+        paint.setPathEffect(null);
     }
 }
